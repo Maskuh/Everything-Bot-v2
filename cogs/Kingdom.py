71040ee70a6ae7cf9405e1922c0ead10bc29b00e
Maskuh <85193434+Maskuh@users.noreply.github.com>
2023-06-01 15:29:29 -0700

    Commit to fix edit

--- cogs/Kingdom.py
+++ cogs/Kingdom.py
@@ -125,8 +125,9 @@ class Kingdom(commands.Cog):
 
     @commands.Cog.listener()
     async def on_raw_reaction_remove(self, payload):
-        print("role was triggered")
         guild = self.client.get_guild(payload.guild_id)
+        if guild.id != 1078694231465541632: return
+        print("role was triggered")
         if payload.emoji.name == "Giveaways" and payload.message_id == self.reaction_roles:
             role = discord.utils.get(guild.roles, name="Gw ping")
             if role is not None:
